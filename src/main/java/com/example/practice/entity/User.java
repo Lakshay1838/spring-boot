5d76f811226c2f6a576a82a2a3b466c2fc5fe0c4
Lakshay <lakshaypahuja777@gmail.com>
2024-12-27 22:03:28 +0530

    added files

---
+++ src/main/java/com/example/practice/entity/User.java
@@ -0,0 +1,19 @@
+package com.example.practice.entity;
+
+
+public class User {
+//    account
+//    name
+//    phone
+//    idtype
+//    idvalue
+//    acctype
+
+    private String name;
+    private String phone;
+    private String idType;
+    private Long idValue;
+    private String accType;
+
+
+}
