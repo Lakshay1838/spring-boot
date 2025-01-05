d216e507bddfe175137aa27e739f957a697addf9
Lakshay <lakshaypahuja777@gmail.com>
2025-01-05 22:54:02 +0530

    added new entity loanType

---
+++ src/main/java/com/example/practice/entity/loanType.java
@@ -0,0 +1,12 @@
+package com.example.practice.entity;
+
+public class loanType {
+
+    private Long id;
+    private String type;
+    private int rateOfInterest;
+    private int time; //in months
+    private boolean isCompound;
+
+
+}
